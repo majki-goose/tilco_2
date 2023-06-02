37ca7c5692c1a944c043d3329b38a0904ec74452
majki-goose <122356102+majki-goose@users.noreply.github.com>
2023-06-02 11:22:29 +0100

    Add files via upload

---
+++ website/js/js.js
@@ -0,0 +1,22 @@
+
+
+
+function temp()
+{
+    alert("hey :)"); 
+}
+
+function small()
+{
+    document.getElementById("body-id").className = "body-small";
+}
+
+function medium()
+{
+    document.getElementById("body-id").className = "body-medium";
+}
+
+function large()
+{
+    document.getElementById("body-id").className = "body-large";
+}
